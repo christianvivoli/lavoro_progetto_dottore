5b22e46e3477193ea187918ceaa79a9d28e94957
Christian Vivoli <christianvivoli04@gmail.com>
2023-09-24 09:02:19 +0200

    primo commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import ReactDOM from 'react-dom/client';
 
-function App() {
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App name="viola" />);
+
+
+
+function App (props){
+
+  const [colore, setColore] = useState("viola");
+  
+console.log(colore);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center >
+      <form className='App-header'>
+      <h1>orari in cui puoi prenotare una visita</h1>
+      <table border={2} cellPadding={25} id='tabella'>
+        <tr id='giorni'>giorni</tr> 
+        <tr><td>orario</td> <td>22/09/2023</td> <td>23/09/2023</td> <td>24/09/2023</td> <td>25/09/2023</td></tr>
+
+    <tr><td>8 AM</td> <td ><button type="" onClick={() =>setColore("arancione")}>{colore}</button></td>    <td>Dato 8</td>     <td>Dato 8</td>     <td>Dato 8</td></tr>
+
+    <tr><td>9 AM</td> <td>Dato 9</td>     <td>Dato 9</td>     <td>Dato 9</td>     <td>Dato 9</td></tr>
+
+    <tr><td>10 AM</td><td>Dato 10</td>    <td>Dato 10</td>    <td>Dato 10</td>    <td>Dato 10</td></tr>
+
+    <tr><td>11 AM</td><td>Dato 11</td>    <td>Dato 11</td>    <td>Dato 11</td>    <td>Dato 11</td></tr>
+
+    <tr><td>12 AM</td><td>Dato 12</td>    <td>Dato 12</td>    <td>Dato 12</td>    <td>Dato 12</td></tr>
+      </table>
+      </form>
+
+    </center>
   );
 }
+
+
 
 export default App;
---
+++ src/components/paginaU.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import React from 'react';
+
+function paginaU() {
+    return (
+        <table>
+            <tr><td>1</td><td>2</td></tr>
+            <tr><td>Dato 1,1</td><td>Dato 1,2</td></tr>
+            <tr><td>Dato 2,1</td><td>Dato 2,2</td></tr>
+            <tr><td>Dato 3,1</td><td>Dato 3,2</td></tr>
+        </table>
+    );
+}
+
+export default paginaU;
